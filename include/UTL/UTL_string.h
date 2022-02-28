6ab486968eac1d5a319fe236fcf98e81ef2466fb
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-02-28 20:45:08 +0100

    add API for utils strings

--- include/UTL/UTL_string.h
+++ include/UTL/UTL_string.h
@@ -1,4 +1,26 @@
 #ifndef UTL_STRING_H
 #define UTL_STRING_H
 
+// self growing string buffer
+typedef struct {
+    int length; // length of content, excluding null terminator
+    int capacity; // capacity of buffer
+    char buf[]; // buffer (null terminated c-string)
+} UTL_String;
+
+// create a new UTL_String and initialze with given c-string
+// leave string empty if @from is null
+extern UTL_String* UTL_CreateString(const char *from);
+
+// free memory of a given UTL_String. returns null
+extern UTL_String* UTL_DestroyString(UTL_String *string);
+
+// make sure the given UTL_String has enough capacity for at least @minLength
+// returns the new string (possible relocation)
+extern UTL_String* UTL_ReserveString(UTL_String *string, int minLength);
+
+// append the contetns of a given c-string to a UTL_String
+// returns the new string (possible relocation)
+extern UTL_String* UTL_AppendToString(UTL_String *string, const char *cstr);
+
 #endif // UTL_STRING_H
---
+++ src/UTL/UTL_string.c
@@ -0,0 +1,17 @@
+#include "UTL/UTL_string.h"
+
+UTL_String* UTL_CreateString(const char *from) {
+
+}
+
+UTL_String* UTL_DestroyString(UTL_String *string) {
+
+}
+
+UTL_String* UTL_ReserveString(UTL_String *string, int minLength) {
+
+}
+
+UTL_String* UTL_AppendToString(UTL_String *string, const char *cstr) {
+    
+}
