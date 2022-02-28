7e9516a0ff58c39e4bbb475456bb1f951dc6d39a
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-02-28 19:18:11 +0100

    fix utils type info struct

--- include/UTL/UTL_common.h
+++ include/UTL/UTL_common.h
@@ -1,11 +1,26 @@
 #ifndef UTL_COMMON_H
 #define UTL_COMMON_H
 
-struct typeInfo {
-    char name[50];
-    int size;
-    char compFct[50];
-    char copyMode[50];
-}
+#include <stdint.h>
+
+// compare function type
+typedef int (UTL_CmpFunc)(const void*, const void*);
+
+// hash function type
+typedef unsigned (UTL_HashFunc)(const void*);
+
+// copy function type
+typedef void (UTL_CopyFunc)(void*, const void*);
+
+// holds information about a type, required for generic containers
+typedef struct {
+    size_t size;
+    UTL_CmpFunc *cmpFunc;
+    UTL_HashFunc *hashFunc;
+    UTL_CopyFunc *copyFunc;
+    const char *name;
+} UTL_TypeInfo;
+
+
 
 #endif // UTL_COMMON_H
