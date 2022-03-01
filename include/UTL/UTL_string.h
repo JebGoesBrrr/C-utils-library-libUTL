1106aa60076073d4d1b0925d05a3ad693a644af2
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-01 14:00:58 +0100

    update documentation for utils string

--- include/UTL/UTL_string.h
+++ include/UTL/UTL_string.h
@@ -1,26 +1,39 @@
 #ifndef UTL_STRING_H
 #define UTL_STRING_H
 
-// self growing string buffer
+
+
+/** self growing string buffer
+ *  stores the string's length, how much space is currently allocated
+ *  and the contents all in one contionous piece of memory */
 typedef struct {
-    int length; // length of content, excluding null terminator
-    int capacity; // capacity of buffer
-    char buf[]; // buffer (null terminated c-string)
+    int length;     /** length of content, excluding null terminator */
+    int capacity;   /** capacity of buffer */
+    char buf[];     /** buffer (null terminated c-string) */
 } UTL_String;
 
-// create a new UTL_String and initialze with given c-string
-// leave string empty if @from is null
+
+
+/** create a new UTL_String and initialze with given c-string
+ *  leaves the new string empty if @from is null
+ *  the retuned string needs to be destroyed with UTL_DestroyString() */
 extern UTL_String* UTL_CreateString(const char *from);
 
-// free memory of a given UTL_String. returns null
+
+/** free memory of a given UTL_String. returns null */
 extern UTL_String* UTL_DestroyString(UTL_String *string);
 
-// make sure the given UTL_String has enough capacity for at least @minLength
-// returns the new string (possible relocation)
+
+/** make sure the given UTL_String has enough capacity for at least @minLength
+ *  returns the new string (possible relocation) */
 extern UTL_String* UTL_ReserveString(UTL_String *string, int minLength);
 
-// append the contetns of a given c-string to a UTL_String
-// returns the new string (possible relocation)
+
+/** append the contetns of a given c-string to a UTL_String
+ *  does nothing if the given string is NULL
+ *  returns the new string (possible relocation) */
 extern UTL_String* UTL_AppendToString(UTL_String *string, const char *cstr);
+
+
 
 #endif // UTL_STRING_H
