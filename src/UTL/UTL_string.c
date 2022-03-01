a42b310ff03e5f3418ba104efad626d4580cc6c8
Wilhelm Simus <wilhelm.simus@gmail.com>
2022-03-01 13:13:51 +0100

    implementation of UTL_CreateString function

--- src/UTL/UTL_string.c
+++ src/UTL/UTL_string.c
@@ -1,7 +1,23 @@
 #include "UTL/UTL_string.h"
 
 UTL_String* UTL_CreateString(const char *from) {
-
+    if (*from == '\0'){
+        UTL_String *s = malloc(sizeof(UTL_String));
+        s->length = 0;
+        s->capacity = 1;
+    }
+    else {
+        char *ptr = from; 
+        int initial_cap = 1;
+        while (*ptr != '\0'){
+            initial_cap += 1;
+            ptr += sizeof(char);
+        };
+        UTL_String *s = malloc(sizeof(UTL_String) + sizeof(char) * initial_cap);
+        s->length = initial_cap -1;
+        s->capacity = initial_cap;
+        // buffer value not set yet
+    }
 }
 
 UTL_String* UTL_DestroyString(UTL_String *string) {
