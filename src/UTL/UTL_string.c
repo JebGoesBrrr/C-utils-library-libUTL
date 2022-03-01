9d00cce0816219fdf8ef7fe0f07eadffe3c4bfa0
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-01 13:29:18 +0100

    fix string creation

--- src/UTL/UTL_string.c
+++ src/UTL/UTL_string.c
@@ -1,23 +1,39 @@
 #include "UTL/UTL_string.h"
 
+#include <string.h>
+
+#define UTL_STRING_INITIAL_CAPACITY 64
+
+static inline int UTL_GrowCapacity(int capacity) {
+    return capacity + (capacity >> 1); // capacity *= 1.5
+}
+
+static int UTL_ComputeNewStringCapacity(int minLength, int currentCapacity) {
+    if (minLength < UTL_STRING_INITIAL_CAPACITY)
+        return UTL_STRING_INITIAL_CAPACITY;
+
+    if (currentCapacity < UTL_STRING_INITIAL_CAPACITY)
+        currentCapacity = UTL_STRING_INITIAL_CAPACITY;
+
+    while (minLength >= currentCapacity)
+        currentCapacity = UTL_GrowCapacity(currentCapacity);
+    
+    return currentCapacity;
+}
+
 UTL_String* UTL_CreateString(const char *from) {
-    if (*from == '\0'){
-        UTL_String *s = malloc(sizeof(UTL_String));
-        s->length = 0;
-        s->capacity = 1;
-    }
-    else {
-        char *ptr = from; 
-        int initial_cap = 1;
-        while (*ptr != '\0'){
-            initial_cap += 1;
-            ptr += sizeof(char);
-        };
-        UTL_String *s = malloc(sizeof(UTL_String) + sizeof(char) * initial_cap);
-        s->length = initial_cap -1;
-        s->capacity = initial_cap;
-        // buffer value not set yet
-    }
+    int fromLength = (from == NULL ? 0 : strlen(from));
+    int capacity   = UTL_ComputeNewStringCapacity(fromLength, 0);
+
+    UTL_String *string = (UTL_String*) malloc(sizeof(UTL_String) + sizeof(char) * capacity);
+
+    string->capacity = capacity;
+    string->length   = fromLength;
+
+    memcpy(string->buf, from, sizeof(char) * fromLength);
+    string->buf[string->length] = 0;
+
+    return string;
 }
 
 UTL_String* UTL_DestroyString(UTL_String *string) {
