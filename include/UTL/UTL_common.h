3c07ddf4025d8b29cf640db983065ca715c44201
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-01 13:54:52 +0100

    add documentation for utils common

--- include/UTL/UTL_common.h
+++ include/UTL/UTL_common.h
@@ -1,6 +1,8 @@
 #ifndef UTL_COMMON_H
 #define UTL_COMMON_H
 
+
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <stddef.h>
@@ -8,31 +10,37 @@
 #include <string.h>
 #include <stdio.h>
 
-// compare function type
+
+
+/** compare function type to be used with generic containers */
 typedef int (UTL_CmpFunc)(const void*, const void*);
 
-// hash function type
+/** hash function type to be used with generic containers */
 typedef unsigned (UTL_HashFunc)(const void*);
 
-// copy function type
+/** copy function type to be used with generic containers */
 typedef void (UTL_CopyFunc)(void*, const void*);
 
-// holds information about a type, required for generic containers
+
+
+/** holds information about a type required for generic containers.
+ *  stores the type's size in bytes, compare function, hash function,
+ *  copy function and name */
 typedef struct {
-    size_t size;
-    UTL_CmpFunc *cmpFunc;
-    UTL_HashFunc *hashFunc;
-    UTL_CopyFunc *copyFunc;
-    const char *name;
+    size_t        size;     /** type's size in bytes */
+    UTL_CmpFunc  *cmpFunc;  /** pointer to type's compare function */
+    UTL_HashFunc *hashFunc; /** pointer to type's hash function */
+    UTL_CopyFunc *copyFunc; /** pointer to type's copy function */
+    const char   *name;     /** type's name */
 } UTL_TypeInfo;
 
 
 
 // type info structs for C's primitive data types
-
 extern UTL_TypeInfo UTL_TypeInfoInt;
 extern UTL_TypeInfo UTL_TypeInfoChar;
 extern UTL_TypeInfo UTL_TypeInfoFloat;
+
 
 
 #endif // UTL_COMMON_H
