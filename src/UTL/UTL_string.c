2c1dcaeddd26d303af5ee6bc2d3134c0884181ab
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-02 13:20:40 +0100

    add documentation of source file

--- src/UTL/UTL_string.c
+++ src/UTL/UTL_string.c
@@ -20,6 +20,11 @@ static int UTL_ComputeNewStringCapacity(int minLength, int currentCapacity) {
     return currentCapacity;
 }
 
+
+/** create a new UTL_String and initialze with given c-string
+ *  leaves the new string empty if @from is null
+ *  will compute length if @length is negative
+ *  the retuned string needs to be destroyed with UTL_DestroyString() */
 UTL_String* UTL_CreateString(const char *cstr, int length) {
     if (cstr == NULL) length = 0;
     if (length < 0) length = strlen(cstr);
@@ -36,11 +41,16 @@ UTL_String* UTL_CreateString(const char *cstr, int length) {
     return string;
 }
 
+
+/** free memory of a given UTL_String. returns null */
 UTL_String* UTL_DestroyString(UTL_String *string) {
     free(string);
     return NULL;
 }
 
+
+/** make sure the given UTL_String has enough capacity for at least @minLength
+ *  returns the new string (possible relocation) */
 UTL_String* UTL_ReserveString(UTL_String *string, int minLength) {
     if (minLength < string->capacity)
         return string;
@@ -50,6 +60,11 @@ UTL_String* UTL_ReserveString(UTL_String *string, int minLength) {
     return string;
 }
 
+
+/** append the contetns of a given c-string to a UTL_String
+ *  does nothing if the given string is NULL
+ *  will compute length if @length is negative
+ *  returns the new string (possible relocation) */
 UTL_String* UTL_AppendToString(UTL_String *string, const char *cstr, int length) {
     if (cstr == NULL) length = 0;
     if (length < 0) length = strlen(cstr);
@@ -62,6 +77,11 @@ UTL_String* UTL_AppendToString(UTL_String *string, const char *cstr, int length)
     return string;
 }
 
+
+/** prepend the contetns of a given c-string to a UTL_String
+ *  does nothing if the given string is NULL
+ *  will compute length if @length is negative
+ *  returns the new string (possible relocation) */
 UTL_String* UTL_PrependToString(UTL_String *string, const char *cstr, int length) {
     if (cstr == NULL) length = 0;
     if (length < 0) length = strlen(cstr);
@@ -75,6 +95,11 @@ UTL_String* UTL_PrependToString(UTL_String *string, const char *cstr, int length
     return string;
 }
 
+
+/** insert the contetns of a given c-string into a UTL_String
+ *  does nothing if the given string is NULL
+ *  will compute length if @length is negative
+ *  returns the new string (possible relocation) */
 UTL_String* UTL_InsertToString(UTL_String *string, int at, const char *cstr, int length) {
     if (at < 0) at = 0;
     if (at > string->length) at = string->length;
@@ -91,6 +116,9 @@ UTL_String* UTL_InsertToString(UTL_String *string, int at, const char *cstr, int
     return string;
 }
 
+
+/** find the first index of any of the given characters in a string, at or after @offset
+ *  returns a negative value of no match is found */
 int UTL_FindFirstOfAnyInString(UTL_String *string, const char *match, int offset) {
     for (int i = offset < 0 ? 0 : offset; i < string->length; i++)
         if (strchr(match, string->buf[i]))
@@ -98,6 +126,9 @@ int UTL_FindFirstOfAnyInString(UTL_String *string, const char *match, int offset
     return -1;
 }
 
+
+/** find the first index of a full match of the given pattern in a string, at or after @offset
+ *  returns a negative value of no match is found */
 int UTL_FindFirstOfAllInString(UTL_String *string, const char *match, int offset) {
     int lenMatch = strlen(match);
     for (int i = offset < 0 ? 0 : offset; i < string->length - lenMatch; i++)
@@ -106,6 +137,9 @@ int UTL_FindFirstOfAllInString(UTL_String *string, const char *match, int offset
     return -1;
 }
 
+
+/** find the last index of any of the given characters in a string, at or before @offset
+ *  returns a negative value of no match is found */
 int UTL_FindLastOfAnyInString(UTL_String *string, const char *match, int offset) {
     for (int i = offset >= string->length ? string->length - 1 : offset; i >= 0; i--)
         if (strchr(match, string->buf[i]))
@@ -113,6 +147,9 @@ int UTL_FindLastOfAnyInString(UTL_String *string, const char *match, int offset)
     return -1;
 }
 
+
+/** find the last index of a full match of the given pattern in a string, at or before @offset
+ *  returns a negative value of no match is found */
 int UTL_FindLastOfAllInString(UTL_String *string, const char *match, int offset) {
     int lenMatch = strlen(match);
     for (int i = offset >= string->length - lenMatch ? string->length - lenMatch - 1 : offset; i >= 0; i--)
