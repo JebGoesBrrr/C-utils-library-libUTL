6f8f79d9a6a7385deadb540c146f2cfc0f180d98
Wilhelm Simus <wilhelm.simus@gmail.com>
2022-03-02 21:24:22 +0100

    Added implementation of UTL_RemoveFromString

--- src/UTL/UTL_string.c
+++ src/UTL/UTL_string.c
@@ -197,12 +197,32 @@ int UTL_FindLastOfAllInString(UTL_String *string, const char *match, int offset)
 /** remove parts of a string
  *  remove everything starting at @first and of length @length */
 void UTL_RemoveFromString(UTL_String *string, int first, int length) {
-    // TODO
+    // snap @first to boundaries
+    if (first < 0) first = 0;
+    if (first >= string->length) return string; // nothing to delete
+
+    // snap removable area to string area
+    int maxRemove = string->length - first;
+    if (length > maxRemove) {
+        // set new length and end of string buffer in order to remove everything starting at @from
+        string->length = first;
+        string->buf[string->length] = 0;
+        return string;
+    }
+    
+    // get size of substring behind removable area
+    int maxCopy = string->length - (first + length);
+    // copy substring at the end into removable area
+    memcpy(string->buf + first, string->buf + first + length, length);
+    // set new length and end of string buffer
+    string->length = first + maxCopy;
+    string->buf[string->length] = 0;
+    return string;
 }
 
 
 /** remove parts of a string
  *  remove everything except content starting at @first and of length @length */
 void UTL_RemoveFromStringRev(UTL_String *string, int first, int length) {
-    // TODO
+    //TODO
 }
