f8a001e2cad29ceb61cac400f40c41b50c4ff915
Alexander Schneiders <alexanderschneiders@protonmail.com>
2022-03-09 01:55:09 +0100

    add conversion to and from pointer

--- include/UTL/UTL_common.h
+++ include/UTL/UTL_common.h
@@ -42,5 +42,11 @@ extern const UTL_TypeInfo UTL_TypeInfoChar;
 extern const UTL_TypeInfo UTL_TypeInfoFloat;
 
 
+// encode a value into the bits of a pointer
+#define UTL_ToPtr(type, val) (((union { type x; void *y; }) { .x = val}).y)
+
+// decode a value from the bits of a pointer
+#define UTL_FromPtr(type, ptr) (((union { type x; void *y;}) { .y = ptr}).x)
+
 
 #endif // UTL_COMMON_H
